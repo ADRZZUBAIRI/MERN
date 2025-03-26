8cc78b4edb47104a371faaec89abe493d9fa22d4
Abdul Rehman Zubairi <associatewithabdulrehman@gmail.com>
2025-03-26 08:46:31 +0500

    commit

--- Day 6/script.js
+++ Day 6/script.js
@@ -9,8 +9,36 @@ document.getElementById("demo2").innerHTML = answer3.charAt(8);
 function myFunction() {
   let test = document.getElementById("demo6");
   if (test.innerHTML.includes("Abdul Rehman")) {
-    test.innerHTML = test.innerHTML.replace("Abdul Rehman", "Abdul Moiz");
-  } else {
-    test.innerHTML = test.innerHTML.replace("Abdul Moiz", "Abdul Rehman");
+    test.innerHTML = test.innerHTML.replace("Abdul Rehman", "Abdul Moiz bin Abdul Rehman");
+    
+  }
+  
+  else if (test.innerHTML.includes("Abdul Moiz bin Abdul Rehman")) {
+      test.innerHTML = test.innerHTML.replace("Abdul Moiz bin Abdul Rehman", "Abdul Rehman");
+    }
+  
+  else {
+    test.innerHTML = test.innerHTML.replace("Abdul Moiz bin Abdul Rehman", "Abdul Rehman");
   }
 }
+
+
+const myarr = ["Alsvin", "Elantra", "Civic", "Vitz", "Corolla", "City", "Cultus", "Picanto", "Swift", "Mehran"];
+myarr[0] = "Audi";
+myarr[1] = "BMW";
+document.getElementById("demo5").innerHTML = myarr;
+document.getElementById("demo6").innerHTML = myarr.length;
+document.getElementById("demo7").innerHTML = myarr.sort();
+document.getElementById("demo8").innerHTML = myarr.reverse();
+document.getElementById("demo9").innerHTML = myarr.indexOf("BMW");
+document.getElementById("demo10").innerHTML = myarr.join(" * ");
+document.getElementById("demo11").innerHTML = myarr.pop();
+document.getElementById("demo12").innerHTML = myarr.push("Mehran");
+document.getElementById("demo13").innerHTML = myarr.shift();
+document.getElementById("demo14").innerHTML = myarr.unshift("Alsvin");
+document.getElementById("demo15").innerHTML = myarr.slice(1, 4);
+document.getElementById("demo16").innerHTML = myarr.splice(2, 0, "Vitz", "Corolla");
+document.getElementById("demo17").innerHTML = myarr;
+document.getElementById("demo18").innerHTML = myarr.splice(2, 2);
+document.getElementById("demo19").innerHTML = myarr;
+document.getElementById("demo20").innerHTML = myarr.concat("Civic", "City");
